b1ff08eb3b396d061c7d209e318017ba4baecf9f
laminthaungtun <laminthaungtun@gmail.com>
2025-09-20 22:01:57 +0630

    Add HelloController for test ping endpoint

--- Controllers/Hello.cs
+++ Controllers/Hello.cs
@@ -3,13 +3,20 @@
 namespace TwoDPro3.Controllers
 {
     [ApiController]
-    [Route("test/[controller]")]
-    public class Hello : ControllerBase
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
     {
+        // GET: https://twodpro3.onrender.com/api/hello/ping?message=Hello
         [HttpGet("ping")]
-        public IActionResult Ping(string message)
+        public IActionResult Ping([FromQuery] string message)
         {
+            if (string.IsNullOrEmpty(message))
+            {
+                return BadRequest("Message cannot be empty.");
+            }
+
             return Ok($"Hi Buddy! You said: {message}");
         }
     }
+
 }
