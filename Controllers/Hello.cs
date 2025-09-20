ce38adf272c57de0b0aee595a43f95174392f7b2
laminthaungtun <laminthaungtun@gmail.com>
2025-09-20 21:31:55 +0630

    Add HelloController for test ping endpoint

---
+++ Controllers/Hello.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TwoDPro3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Hello : Controller
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping(string message)
+        {
+            if (message == "Hello")
+                return Ok("Hi Buddy");
+            else
+                return Ok("I only reply to Hello :)");
+        }
+    }
+}
