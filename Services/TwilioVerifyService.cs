42c7bd01971a970c027f3da995aa3558b17f3f69
laminthaungtun <laminthaungtun@gmail.com>
2026-01-21 17:23:04 +0630

    Add  Services/TwilioVerifyService.cs

---
+++ Services/TwilioVerifyService.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+public class TwilioVerifyService
+{
+    private readonly HttpClient _http;
+    private readonly string _verifySid;
+    private readonly string _accountSid;
+    private readonly string _authToken;
+
+    public TwilioVerifyService(HttpClient http, IConfiguration config)
+    {
+        _http = http;
+        _verifySid = config["Twilio:VerifyServiceSid"];
+        _accountSid = config["Twilio:AccountSid"];
+        _authToken = config["Twilio:AuthToken"];
+
+        var authBytes = Encoding.ASCII.GetBytes($"{_accountSid}:{_authToken}");
+        _http.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authBytes));
+    }
+
+    public async Task<bool> SendOtpAsync(string phone)
+    {
+        var content = new FormUrlEncodedContent(new[]
+        {
+            new KeyValuePair<string, string>("To", phone),
+            new KeyValuePair<string, string>("Channel", "sms")
+        });
+
+        var response = await _http.PostAsync(
+            $"https://verify.twilio.com/v2/Services/{_verifySid}/Verifications",
+            content
+        );
+
+        return response.IsSuccessStatusCode;
+    }
+    public async Task<bool> VerifyOtpAsync(string phone, string code)
+    {
+        var content = new FormUrlEncodedContent(new[]
+        {
+        new KeyValuePair<string, string>("To", phone),
+        new KeyValuePair<string, string>("Code", code)
+    });
+
+        var response = await _http.PostAsync(
+            $"https://verify.twilio.com/v2/Services/{_verifySid}/VerificationCheck",
+            content
+        );
+
+        return response.IsSuccessStatusCode;
+    }
+
+}
