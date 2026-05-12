a8ee062f0d1f2e44f016300adc2197f3e6456b84
laminthaungtun <laminthaungtun@gmail.com>
2026-05-13 00:50:14 +0630

    Ass OtpCode.cs

---
+++ Models/OtpCode.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TwoDPro3.Models
+{
+    public class OtpCode
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public long TelegramChatId { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Code { get; set; } = "";
+
+        public DateTime ExpiresAt { get; set; }
+
+        public bool IsUsed { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
