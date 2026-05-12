bec8810f3cbf353a90e2090711b0e1f4595f7ab5
laminthaungtun <laminthaungtun@gmail.com>
2026-05-13 00:51:10 +0630

    Add UserTelegramLink

---
+++ Models/UserTelegramLink.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TwoDPro3.Models
+{
+    public class UserTelegramLink
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string PhoneNumber { get; set; } = "";
+
+        public long TelegramChatId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
